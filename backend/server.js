6cbf3af9c0674a3526a802855dbdde8f26988fd5
Reddi-Jyothibhaskar <reddijyothibhaskar@gmail.com>
2025-07-26 17:10:22 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,9 @@ const cors = require('cors');
 
 require('dotenv').config(); 
 const app = express();
-app.use(cors());
+app.use(cors(
+    origin: 'https://college-news.vercel.app'
+));
 app.use(express.json());
 
 // TODO : Add routes here later
